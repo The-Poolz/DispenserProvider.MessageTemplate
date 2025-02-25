ad2a1ab5dfead619b9a89134a836819ef773ac89
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-02-25 18:35:54 +0300

    Use the latest version of `TokenSchedule.FluentValidation` package (#24)

--- csharp/tests/DispenserProvider.MessageTemplate.Tests/Mocks/MockValidatedScheduleItem.cs
+++ csharp/tests/DispenserProvider.MessageTemplate.Tests/Mocks/MockValidatedScheduleItem.cs
@@ -1,28 +1,22 @@
-﻿using Nethereum.Util;
-using System.Numerics;
+﻿using System.Numerics;
 using TokenSchedule.FluentValidation.Models;
 
 namespace DispenserProvider.MessageTemplate.Tests.Mocks;
 
-public class MockValidatedScheduleItem(decimal ratio, DateTime startDate, DateTime? finishDate) : IValidatedScheduleItem
+public class MockValidatedScheduleItem(BigInteger ratio, DateTime startDate, DateTime? finishDate) : IValidatedScheduleItem
 {
-    public MockValidatedScheduleItem(decimal ratio, long startDate, long? finishDate)
-        : this(ratio, DateTimeOffset.FromUnixTimeSeconds(startDate).UtcDateTime, !finishDate.HasValue ? null : DateTimeOffset.FromUnixTimeSeconds(finishDate.Value).UtcDateTime)
-    { }
-
-    public MockValidatedScheduleItem(BigInteger ratio, BigInteger startDate, BigInteger finishDate)
-        : this(
-            UnitConversion.Convert.FromWei(ratio, 18),
+    public MockValidatedScheduleItem(BigInteger ratio, BigInteger startDate, BigInteger? finishDate)
+        : this(ratio,
             DateTimeOffset.FromUnixTimeSeconds((long)startDate).UtcDateTime,
-            finishDate == 0 ? null : DateTimeOffset.FromUnixTimeSeconds((long)finishDate).UtcDateTime
+            finishDate > 0 ? DateTimeOffset.FromUnixTimeSeconds((long)finishDate.Value).UtcDateTime : null
         )
     { }
 
-    public MockValidatedScheduleItem(string ratio, BigInteger startDate, BigInteger finishDate)
+    public MockValidatedScheduleItem(string ratio, BigInteger startDate, BigInteger? finishDate)
         : this(BigInteger.Parse(ratio), startDate, finishDate)
     { }
 
-    public decimal Ratio { get; } = ratio;
+    public BigInteger Ratio { get; } = ratio;
     public DateTime StartDate { get; } = startDate;
     public DateTime? FinishDate { get; } = finishDate;
 }

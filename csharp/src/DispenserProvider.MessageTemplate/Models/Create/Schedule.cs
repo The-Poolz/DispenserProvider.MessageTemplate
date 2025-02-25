6a3a6fd0b0969ee9c221d5ac97c0e90d10463829
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-02-25 18:25:52 +0300

    Use `uint256` instead of `string` for `Ratio` and `Amount` in the EIP712 message (#22)
    * Use `uint256` instead of `string` for `Ratio` and `Amount` in the EIP712 message
    
    * - update docs
    
    * - update version to 1.0.6

--- csharp/src/DispenserProvider.MessageTemplate/Models/Create/Schedule.cs
+++ csharp/src/DispenserProvider.MessageTemplate/Models/Create/Schedule.cs
@@ -6,17 +6,17 @@ using Nethereum.ABI.FunctionEncoding.Attributes;
 namespace DispenserProvider.MessageTemplate.Models.Create;
 
 [Struct(name: "schedules")]
-public class Schedule(EthereumAddress providerAddress, string ratio, long startDate, long finishDate)
+public class Schedule(EthereumAddress providerAddress, BigInteger weiRatio, long startDate, long finishDate)
 {
-    public Schedule(EthereumAddress providerAddress, string ratio, DateTime startDate, DateTime finishDate)
-        : this(providerAddress, ratio, startDate.ToUnixTimestamp(), finishDate.ToUnixTimestamp())
+    public Schedule(EthereumAddress providerAddress, BigInteger weiRatio, DateTime startDate, DateTime finishDate)
+        : this(providerAddress, weiRatio, startDate.ToUnixTimestamp(), finishDate.ToUnixTimestamp())
     { }
 
     [Parameter(type: "address", name: "providerAddress", order: 1)]
     public string ProviderAddress { get; } = providerAddress;
 
-    [Parameter(type: "string", name: "weiRatio", order: 2)]
-    public string Ratio { get; } = ratio;
+    [Parameter(type: "uint256", name: "weiRatio", order: 2)]
+    public BigInteger WeiRatio { get; } = weiRatio;
 
     [Parameter(type: "uint256", name: "startTime", order: 3)]
     public BigInteger StartDate { get; } = startDate;
--- csharp/src/DispenserProvider.MessageTemplate/Models/Create/User.cs
+++ csharp/src/DispenserProvider.MessageTemplate/Models/Create/User.cs
@@ -1,14 +1,15 @@
-﻿using Net.Web3.EthereumWallet;
+﻿using System.Numerics;
+using Net.Web3.EthereumWallet;
 using Nethereum.ABI.FunctionEncoding.Attributes;
 
 namespace DispenserProvider.MessageTemplate.Models.Create;
 
 [Struct(name: "users")]
-public class User(EthereumAddress userAddress, string weiAmount)
+public class User(EthereumAddress userAddress, BigInteger weiAmount)
 {
     [Parameter(type: "address", name: "userAddress", order: 1)]
     public string UserAddress { get; } = userAddress;
 
-    [Parameter(type: "string", name: "weiAmount", order: 2)]
-    public string WeiAmount { get; } = weiAmount;
+    [Parameter(type: "uint256", name: "weiAmount", order: 2)]
+    public BigInteger WeiAmount { get; } = weiAmount;
 }

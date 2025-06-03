74c7b6ee2a5e5f1991a1c7071d4b2422926801f4
ArdenHide <ardenhide@gmail.com>
2025-06-03 08:23:07 +0300

    Use `IReadOnlyCollection` in `IAdminValidationService.IsValidAdmin(string userAddress, IEnumerable<long> chainIDs)`

--- csharp/src/DispenserProvider.MessageTemplate.Examples/MockAdminValidationService.cs
+++ csharp/src/DispenserProvider.MessageTemplate.Examples/MockAdminValidationService.cs
@@ -8,7 +8,7 @@ internal class MockAdminValidationService : IAdminValidationService
     internal static EthECKey AdminPrivateKey => new("0xe290f27189205c280e53b6194580c24b4fc49c97047b5ffcb62201c4d38feece");
     internal static string AdminAddress => AdminPrivateKey.GetPublicAddress();
 
-    public bool IsValidAdmin(string userAddress, IEnumerable<long> chainIDs)
+    public bool IsValidAdmin(string userAddress, IReadOnlyCollection<long> chainIDs)
     {
         return userAddress == AdminAddress;
     }
--- csharp/src/DispenserProvider.MessageTemplate/Models/Validators/AdminRequestValidatorSettings.cs
+++ csharp/src/DispenserProvider.MessageTemplate/Models/Validators/AdminRequestValidatorSettings.cs
@@ -4,9 +4,9 @@ using DispenserProvider.MessageTemplate.Models.Eip712;
 
 namespace DispenserProvider.MessageTemplate.Models.Validators;
 
-public class AdminRequestValidatorSettings(string signature, AbstractMessage message, IEnumerable<long> chainIDs)
+public class AdminRequestValidatorSettings(string signature, AbstractMessage message, IReadOnlyCollection<long> chainIDs)
 {
-    public IEnumerable<long> ChainIDs { get; } = chainIDs;
+    public IReadOnlyCollection<long> ChainIDs { get; } = chainIDs;
     public string Signature { get; } = signature;
     public AbstractMessage Message { get; } = message;
 
